3ca1ba599677e98ae8cad3cab738177cf7b22e71
Badz110m <baderturkai100@gmail.com>
2023-07-19 19:15:56 -0400

    solved 4-7

--- problems/04-my-filter.js
+++ problems/04-my-filter.js
@@ -20,12 +20,28 @@ console.log(result2);      // ['choose', 'words', 'only']
 *******************************************************************************/
 
 function myFilter(array, cb) {
-    // Your code here
+    let newArr = []
+    for (let i = 0; i < array.length; i++) {
+        if (cb(array[i])) {
+newArr.push(array[i])
+        }
+    }
+    return newArr
 }
+
+let result1 = myFilter([5, 7, 4, 3, 8], function (n) {
+    return n % 2 === 0;
+});
+console.log(result1);       // [ 4, 8 ]
+
+let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
+    return s.length > 3;
+});
+console.log(result2);      // ['choose', 'words', 'only']
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
     module.exports = myFilter;
-} catch(e) {
+} catch (e) {
     return null;
 }
